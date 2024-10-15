541f688d761f518c8a4d20aeb0530ccdf7e629ca
ByScream_ <mathis.richard@sfr.fr>
2024-10-15 19:26:37 +0200

    Mise à jour BDD + ERD

--- database/if3e_projet_b.sql
+++ database/if3e_projet_b.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 07 oct. 2024 à 11:40
+-- Généré le : mar. 15 oct. 2024 à 19:25
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -40,9 +40,9 @@ CREATE TABLE `games` (
 --
 
 INSERT INTO `games` (`id`, `game_name`, `type`, `rules`, `number_of_players`) VALUES
-(1, 'chess', 0, '', 0),
-(2, 'Settlers of Catan', 0, '', 0),
-(3, 'Ticket to Ride', 0, '', 0),
+(1, 'chess', 0, '', 2),
+(2, 'Settlers of Catan', 0, '', 2),
+(3, 'Ticket to Ride', 0, '', 2),
 (4, 'Monopoly', 1, 'bla bla bla', 2);
 
 -- --------------------------------------------------------
@@ -97,13 +97,8 @@ CREATE TABLE `player_team` (
 --
 
 INSERT INTO `player_team` (`id_user`, `id_team`) VALUES
-(2, 2),
-(5, 2),
-(1, 2),
-(4, 2),
-(3, 1),
-(5, 1),
-(1, 1);
+(10, 8),
+(10, 9);
 
 -- --------------------------------------------------------
 
@@ -122,8 +117,8 @@ CREATE TABLE `teams` (
 --
 
 INSERT INTO `teams` (`team_id`, `team_name`, `owner`) VALUES
-(1, 'Les fifous', 1),
-(2, 'Kiki family', 5);
+(8, 'Richard Team', 10),
+(9, 'Uttewiller company', 10);
 
 -- --------------------------------------------------------
 
@@ -133,7 +128,8 @@ INSERT INTO `teams` (`team_id`, `team_name`, `owner`) VALUES
 
 CREATE TABLE `tournament` (
   `id` int(11) NOT NULL,
-  `game_id` int(11) NOT NULL,
+  `game_id` int(11) DEFAULT NULL,
+  `name` varchar(256) NOT NULL,
   `number_of_players` int(11) NOT NULL,
   `match_rules` text NOT NULL,
   `owner` int(11) NOT NULL,
@@ -144,9 +140,10 @@ CREATE TABLE `tournament` (
 -- Déchargement des données de la table `tournament`
 --
 
-INSERT INTO `tournament` (`id`, `game_id`, `number_of_players`, `match_rules`, `owner`, `type`) VALUES
-(1, 4, 2, 'Match de 2 équipes', 1, 1),
-(2, 3, 3, '', 3, 0);
+INSERT INTO `tournament` (`id`, `game_id`, `name`, `number_of_players`, `match_rules`, `owner`, `type`) VALUES
+(7, 4, 'Tournoi d\'Héricourt', 3, 'Elimination directe', 10, 1),
+(8, 1, 'Tournoi de Belfort', 2, 'Elimination directe', 10, 0),
+(9, 4, 'Championnat BFC', 5, 'eee', 10, 0);
 
 -- --------------------------------------------------------
 
@@ -164,8 +161,7 @@ CREATE TABLE `tournaments_teams` (
 --
 
 INSERT INTO `tournaments_teams` (`team`, `tournament_id`) VALUES
-(1, 1),
-(2, 1);
+(8, 7);
 
 -- --------------------------------------------------------
 
@@ -183,9 +179,8 @@ CREATE TABLE `tournament_players` (
 --
 
 INSERT INTO `tournament_players` (`tournament_id`, `player`) VALUES
-(2, 1),
-(2, 3),
-(2, 2);
+(8, 12),
+(8, 13);
 
 -- --------------------------------------------------------
 
@@ -195,26 +190,27 @@ INSERT INTO `tournament_players` (`tournament_id`, `player`) VALUES
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `email` text NOT NULL,
+  `email` varchar(256) NOT NULL,
   `password` text NOT NULL,
   `pseudo` text NOT NULL,
   `prenom` text NOT NULL,
   `nom` text NOT NULL,
   `phone_number` int(10) NOT NULL,
-  `favourite_game_id` int(2) NOT NULL,
-  `date_of_birth` date DEFAULT current_timestamp()
+  `favourite_game_id` int(2) DEFAULT NULL,
+  `date_of_birth` date DEFAULT current_timestamp(),
+  `is_organizer` int(2) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `pseudo`, `prenom`, `nom`, `phone_number`, `favourite_game_id`, `date_of_birth`) VALUES
-(1, 'mathis.richard@sfr.fr', 'abc', 'ByScream_', 'Mathis', 'Richard', 616960870, 1, '2005-03-14'),
-(2, 'jerome.richard@sfr.fr', 'abcde', 'GegeDu70', 'Jérôme', 'Richard', 383, 1, '1977-04-26'),
-(3, 'juline.uttewiller@gmail.com', 'aaa', 'Jul!ne', 'Juline', 'Uttewiller', 33838, 3, '2006-09-11'),
-(4, 'veronique.richard@sfr.fr', 'bbb', 'Vero', 'Véronique', 'Richard', 32, 2, '1976-11-22'),
-(5, 'kylian.richard@gmail.com', 'ccc', 'Vitrax', 'Kylian', 'Richard', 33372, 3, '2009-10-06');
+INSERT INTO `users` (`id`, `email`, `password`, `pseudo`, `prenom`, `nom`, `phone_number`, `favourite_game_id`, `date_of_birth`, `is_organizer`) VALUES
+(10, 'mathis.richard@sfr.fr', '$2y$10$u3RvAalYLqquqTKRaOLpBeXd1uAUBrVrAC2CVTjYvE3pkMKnRxeFi', 'ByScream_', 'Mathis', 'Richard', 616960870, 1, '2005-02-14', 1),
+(11, 'kylian.richard@gmail.com', '$2y$10$5viVvMfK4/v9yHldlY3OOeeikc5Yb8OIz.BZJm99wIVKyAWrTAJ6e', 'Vitrax', 'Kylian', 'Richard', 0, 2, '2009-10-06', 0),
+(12, 'veronique.richard@sfr.fr', '$2y$10$tcKV7okQj/sPtupAzjDUVuWUrcedCxlqlS4SN3befXlG9DPiQPLm.', 'Vero70', 'Véronique', 'Richard', 0, 1, '1976-11-22', 0),
+(13, 'jeromerichard69@sfr.fr', '$2y$10$go5AZzyC14ulLEiT39hIh..kyB0LsnjaItXaEH.XpEG1QoI0P3iWS', 'Gege70', 'Jérôme', 'Richard', 0, 4, '1977-04-26', 0),
+(14, 'juline.uttewiller@gmail.com', '$2y$10$sk3u//hyiuEIJNwjr/WoNehHlhwbV.e.36k4u/BEeRxYRl6aUPtke', 'Jul!ne', 'Juline', 'Richard', 0, 1, '2006-09-11', 0);
 
 --
 -- Index pour les tables déchargées
@@ -230,9 +226,9 @@ ALTER TABLE `games`
 -- Index pour la table `match_player`
 --
 ALTER TABLE `match_player`
-  ADD KEY `tournament_id` (`tournament_id`,`player1`,`player2`),
   ADD KEY `player1` (`player1`),
-  ADD KEY `player2` (`player2`);
+  ADD KEY `player2` (`player2`),
+  ADD KEY `tournament_id` (`tournament_id`);
 
 --
 -- Index pour la table `match_team`
@@ -246,8 +242,9 @@ ALTER TABLE `match_team`
 -- Index pour la table `player_team`
 --
 ALTER TABLE `player_team`
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_team` (`id_team`);
+  ADD UNIQUE KEY `id_user_2` (`id_user`,`id_team`),
+  ADD KEY `id_team` (`id_team`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Index pour la table `teams`
@@ -261,13 +258,14 @@ ALTER TABLE `teams`
 --
 ALTER TABLE `tournament`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `game_id` (`game_id`),
-  ADD KEY `owner` (`owner`);
+  ADD KEY `owner` (`owner`),
+  ADD KEY `game_id` (`game_id`);
 
 --
 -- Index pour la table `tournaments_teams`
 --
 ALTER TABLE `tournaments_teams`
+  ADD UNIQUE KEY `team_2` (`team`,`tournament_id`),
   ADD KEY `tournament_id` (`tournament_id`),
   ADD KEY `team` (`team`);
 
@@ -275,6 +273,7 @@ ALTER TABLE `tournaments_teams`
 -- Index pour la table `tournament_players`
 --
 ALTER TABLE `tournament_players`
+  ADD UNIQUE KEY `tournament_id_2` (`tournament_id`,`player`),
   ADD KEY `tournament_id` (`tournament_id`),
   ADD KEY `player` (`player`);
 
@@ -282,7 +281,8 @@ ALTER TABLE `tournament_players`
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `favourite_game_id` (`favourite_game_id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -292,25 +292,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `games`
 --
 ALTER TABLE `games`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `tournament`
 --
 ALTER TABLE `tournament`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Contraintes pour les tables déchargées
@@ -349,8 +349,8 @@ ALTER TABLE `teams`
 -- Contraintes pour la table `tournament`
 --
 ALTER TABLE `tournament`
-  ADD CONSTRAINT `tournament_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`),
-  ADD CONSTRAINT `tournament_ibfk_2` FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `tournament_ibfk_2` FOREIGN KEY (`owner`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `tournament_ibfk_3` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`);
 
 --
 -- Contraintes pour la table `tournaments_teams`
@@ -365,6 +365,12 @@ ALTER TABLE `tournaments_teams`
 ALTER TABLE `tournament_players`
   ADD CONSTRAINT `tournament_players_ibfk_1` FOREIGN KEY (`player`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `tournament_players_ibfk_2` FOREIGN KEY (`tournament_id`) REFERENCES `tournament` (`id`);
+
+--
+-- Contraintes pour la table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`favourite_game_id`) REFERENCES `games` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
